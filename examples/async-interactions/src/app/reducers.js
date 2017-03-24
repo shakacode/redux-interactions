b1ed1cbbf87a1902e0627c1be6e4b16e36679dd0
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-03-24 22:32:17 +0800

    Remove stale reducers

--- examples/async-interactions/src/app/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-
-import dataReducers from './entities/reducers';
-import postsSectionReducers from './ui/sections/posts/reducers';
-
-export default combineReducers({
-  data: dataReducers,
-  postsSection: postsSectionReducers,
-});
